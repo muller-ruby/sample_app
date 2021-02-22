259509104c62ac00183dbf0a3919d961d0565765
muller-ruby <14muller@gmail.com>
2021-02-22 11:44:11 +0000

    Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,19 @@ class SessionsController < ApplicationController
   end
   
   def create
+    # ユーザーがデータベースにあり、かつ、認証に成功した場合にのみログインできる
     user = User.find_by(email: params[:session][:email].downcase)
-     # ユーザーがデータベースにあり、かつ、認証に成功した場合にのみログインできる
      #　authenticateメソッドは認証に失敗した時にfalseを返す(6.3.4)
     if user && user.authenticate(params[:session][:password])
       #　ユーザーログイン後にユーザー情報のページにリダイレクトする
+      #　ヘルパーメソッドでlog_inを定義したので、以下の通りかける。
       log_in user
-      #　ヘルパーメソッドでlog_inを定義したので、上記の通りかける。
+      # # model/user.rbのdef authenticated?(remember_token)実装の関連で
+      # #　ログインと連携させた（9.7）
+      # remember user
+      # paramsハッシュ値を調べれば、送信された値に基づいてユーザーを記憶したり、
+      # 忘れたりできるようになる。（三項演算子：if-thenの分岐構造）
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
@@ -19,7 +25,11 @@ class SessionsController < ApplicationController
   end
   
   def destroy
-    log_out
+    # test/integration/users_login_test.rbをした結果、
+    # current_userがないために、2回目（user && user.authenticated?(cookies[:remember_token])
+    # logged_in?がtrueの場合に限ってlog_outを呼び出すように変更する
+    #例：ブラウザで複数のタブで開いていて、1つのタブでログアウトして、もう一つのタブでログアウトしようとするとエラーになる
+    log_out if logged_in?
     redirect_to root_url
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,16 +4,42 @@ module SessionsHelper
     session[:user_id] = user.id
   end
   
-  # 現在ログイン中のユーザを返す（いる場合）.8.2.2参照
+  #　ユーザーのセッションを永続的にする
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+  
+  # 記憶トークンcookiesに対応するユーザーを返す
   def current_user
-    if session[:user_id]
-      #　Userオブジェクトそのものの論理値は常にtrueになる。
-      #　そのおかげで、@current_userに何も代入されていないときだけ
-      #　find_by呼び出しが実行され、無駄なデータベースへの読み出しが
-      #　行われなくなる。（||＝は、or equals)
-      @current_user ||= User.find_by(id: session[:user_id])
+        # （ユーザーidにユーザーidのセッションを代入した結果）
+        # ユーザーIDのセッションが存在すれば（9.9）
+        # 「＝」は比較ではなく、代入を行っている。
+    if(user_id = session[:user_id])
+        #　Userオブジェクトそのものの論理値は常にtrueになる。
+        #　そのおかげで、@current_userに何も代入されていないときだけ
+        #　find_by呼び出しが実行され、無駄なデータベースへの読み出しが
+        #　行われなくなる。（||＝は、or equals)
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      #raise   # テストがパスすれば、この部分がテストされていないことがわかる
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
+  
+  # 現在ログイン中のユーザを返す（いる場合）.8.2.2参照
+  # def current_user
+  #   if session[:user_id]
+  #  
+  #     @current_user ||= User.find_by(id: session[:user_id])
+  #   end
+  # end
+ 
   
   # ユーザーがログインしていればtrue,その他ならfalseを返す
   def logged_in?
@@ -21,8 +47,17 @@ module SessionsHelper
     !current_user.nil?
   end
   
+   
+  # 永続的にセッションを破棄する
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+  
   # 現在のユーザーをログアウトする
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
